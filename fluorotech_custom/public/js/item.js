894330be4e6d9b3fd3d9623f0538a8f8ffbe7c11
truptihybrowlabs <trupti@mail.hybrowlabs.com>
2026-04-09 12:07:35 +0530

    |#fluorotech_custom|#Added mandatory depends on|

--- fluorotech_custom/public/js/item.js
+++ fluorotech_custom/public/js/item.js
@@ -1,11 +1,14 @@
 frappe.ui.form.on('Item', {
     onload: function(frm) {
-        load_raw_material_options(frm);
+        if (frm.is_new()) {
+            frm.set_value('inspection_required_before_purchase', 1);
+        }
+        // load_raw_material_options(frm);
     },
 
-    refresh: function(frm) {
-        load_raw_material_options(frm);
-    },
+    // refresh: function(frm) {
+    //     load_raw_material_options(frm);
+    // },
 
     has_batch_no: function(frm) {
         if (frm.doc.has_batch_no) {
@@ -26,29 +29,29 @@ frappe.ui.form.on('Item Customer Detail', {
     }
 });
 
-function load_raw_material_options(frm) {
-    frappe.call({
-        method: 'frappe.client.get_list',
-        args: {
-            doctype: 'Item',
-            filters: [['item_group', '=', 'Raw Material']],
-            fields: ['item_name'],
-            order_by: 'item_name asc',
-            limit_page_length: 0
-        },
-        callback: function(r) {
-            if (r.message) {
-                let options = [''];
-                r.message.forEach(function(item) {
-                    options.push(item.item_name);
-                });
-                options.sort((a, b) => a.localeCompare(b));
-                frm.set_df_property(
-                    'custom_combine_material_name',
-                    'options',
-                    options.join('\n')
-                );
-            }
-        }
-    });
-}
+// function load_raw_material_options(frm) {
+//     frappe.call({
+//         method: 'frappe.client.get_list',
+//         args: {
+//             doctype: 'Item',
+//             filters: [['item_group', '=', 'Raw Material']],
+//             fields: ['item_name'],
+//             order_by: 'item_name asc',
+//             limit_page_length: 0
+//         },
+//         callback: function(r) {
+//             if (r.message) {
+//                 let options = [''];
+//                 r.message.forEach(function(item) {
+//                     options.push(item.item_name);
+//                 });
+//                 options.sort((a, b) => a.localeCompare(b));
+//                 frm.set_df_property(
+//                     'custom_combine_material_name',
+//                     'options',
+//                     options.join('\n')
+//                 );
+//             }
+//         }
+//     });
+// }
