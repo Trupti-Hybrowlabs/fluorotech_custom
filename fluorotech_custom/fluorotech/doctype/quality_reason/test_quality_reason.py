1f40ee77854b51b76c9dc57a1020212b9f8b72d9
truptihybrowlabs <trupti@mail.hybrowlabs.com>
2026-08-24 10:33:06 +0530

    |Created Quality reason doctype|

---
+++ fluorotech_custom/fluorotech/doctype/quality_reason/test_quality_reason.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Trupti Ninghot and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQualityReason(FrappeTestCase):
+	pass
